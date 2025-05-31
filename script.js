7362796722d28ed048f325b3c09d18d5ebb22dbd
hellafi1992 <121656343+hellafi1992@users.noreply.github.com>
2025-05-31 20:22:42 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function ShoWs1() {
+    document.getElementById("img_center") . src =
+    "image1.jpg"
+}
+function ShoWs2(){
+    document.getElementById("img_center"). src =
+    "image2.jpg"
+}
+
+
+
